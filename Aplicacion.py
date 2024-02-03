963659a21e092fb0f2405dfab1ec3c0619ec5d7f
Luis <142854556+AstroLui@users.noreply.github.com>
2024-02-03 16:03:28 -0400

    feat: adding the `Game Variables`

--- Aplicacion.py
+++ Aplicacion.py
@@ -29,17 +29,30 @@ def draw_text(text, font, text_color, x, y):
 #Widgets
 Button_Option = Button('Opciones', 250, 50, (260, 400), font_widgets)
 
+#Game Variables
+game_option = False
+game_start = False
+
 #Game Loop
 while running: 
     screen.fill('#DCDDD8')
-    draw_text("Juego de la Vida",font, '#000000', 75, 150)
-    draw_text("de Conway", font_other, '#000000', 90, 190)
-    draw_text('Presione SPACE para comenzar', font_other, '#000000', 110, 340)
-    Button_Option.draw(screen)
+    if Button_Option.pressed == True:
+        pass
+    elif game_start == True:
+        pass
+    else:
+        draw_text("Juego de la Vida",font, '#000000', 75, 150)
+        draw_text("de Conway", font_other, '#000000', 90, 190)
+        draw_text('Presione SPACE para comenzar', font_other, '#000000', 110, 340)
+        Button_Option.draw(screen)
+    
     # Event Handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                game_start = True
 
     pygame.display.update()
     
