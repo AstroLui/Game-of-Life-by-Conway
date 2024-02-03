e1219401bafcd75e1ab2cd600c8a3838c419358e
Luis <142854556+AstroLui@users.noreply.github.com>
2024-02-03 19:43:13 -0400

    feat: adding the new Widget named `ButtonImage`

---
+++ Widgets/ButtonImage.py
@@ -0,0 +1,53 @@
+import pygame
+
+class ButtonImg():
+    def __init__(self, width: int, height: int, pos: int, image: pygame.surface.SurfaceType, scale: int, elevation: int):
+        #Core Actribute
+        
+        self.pressed = False
+        self.elevation = elevation
+        self.dinamyc_elevation = elevation
+        self.original_pos = pos[1]
+
+        #top rectagular
+        self.top_rect = pygame.Rect(pos, (width, height))
+        self.top_color = '#FFFFFF'
+
+        #bottom rectangular
+        self.bottom_rect = pygame.Rect(pos, (width, elevation))
+        self.bottom_color = '#606060'
+
+        #Image
+        Width = image.get_width()
+        Height = image.get_height()
+        self.Image = pygame.transform.scale(image, (Width * scale, Height * scale))
+        self.ImageRect = self.Image.get_rect(center=self.top_rect.center)
+
+    def draw(self, screen): 
+        #Elevacion
+        self.top_rect.y = self.original_pos - self.dinamyc_elevation
+        self.ImageRect.center = self.top_rect.center
+
+        self.bottom_rect.midtop = self.top_rect.midtop
+        self.bottom_rect.height = self.top_rect.height + self.dinamyc_elevation
+
+        pygame.draw.rect(screen, self.bottom_color, self.bottom_rect, border_radius= 12 )
+        pygame.draw.rect(screen, self.top_color, self.top_rect, border_radius= 12 )
+        screen.blit(self.Image, self.ImageRect)
+        self.checked_click()
+
+    def checked_click(self):
+        moused_pos = pygame.mouse.get_pos()
+        if self.top_rect.collidepoint(moused_pos):
+            self.top_color = '#C6C6C6'
+            if pygame.mouse.get_pressed()[0]:
+                self.dinamyc_elevation = 0
+                self.pressed = True
+            else:
+                self.dinamyc_elevation = self.elevation
+                if self.pressed == True: 
+                    print('Click')
+                    self.pressed = False
+        else:
+            self.dinamyc_elevation = self.elevation
+            self.top_color = 'white'
