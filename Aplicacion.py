a3240b964d17a7ee4b5d1b84d0dc6e6622f7ed5d
Luis <142854556+AstroLui@users.noreply.github.com>
2024-02-03 19:44:07 -0400

    feat: adding the Buttons for MenuOption

--- Aplicacion.py
+++ Aplicacion.py
@@ -1,6 +1,7 @@
 import pygame
 from Widgets.Button import *
 from Widgets.Text import *
+from Widgets.ButtonImage import *
 
 #Iniciamos Pygame
 pygame.init()
@@ -25,11 +26,17 @@ font_widgets = pygame.font.Font('Fonts/Sixtyfour.TTF', 15)
 #Funciones
 
 #Widgets
+    #Widgets de la Pantalla Principal
 Button_Option = Button('Opciones', 250, 50, (260, 400), font_widgets, 6)
 TitleCenter = Text('Juego de la Vida', font, '#000000', 75, 150)
 SubTitle = Text('de Conway', font_other, '#000000', 90, 190)
 StartText = Text('Presione SPACE para comenzar', font_other, '#000000', 110, 340)
 
+    #Widgets de las Opciones
+FlechaIzq = pygame.image.load('Image/FlechaIzq.png').convert_alpha()
+Button_FlechaIzq = ButtonImg(50, 50, (20, 20),FlechaIzq, 0.07, 6)
+FlechaDer = pygame.image.load('Image/FlechaDer.png').convert_alpha()
+Button_FlechaDer = ButtonImg(50, 50, (90, 20), FlechaDer, 0.07, 6)
 #Game Variables
 game_option = False
 game_start = False
@@ -38,6 +45,8 @@ game_start = False
 while running: 
     screen.fill('#DCDDD8')
     if Button_Option.pressed == True:
+        Button_FlechaIzq.draw(screen)
+        Button_FlechaDer.draw(screen)
         pass
     elif game_start == True:
         pass
