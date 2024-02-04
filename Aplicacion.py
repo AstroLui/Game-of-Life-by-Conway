a5d838040214f9f4502386e252273e903538a634
Luis <142854556+AstroLui@users.noreply.github.com>
2024-02-04 14:52:50 -0400

    feat: adding the avance the game

--- Aplicacion.py
+++ Aplicacion.py
@@ -25,8 +25,6 @@ font_other = pygame.font.Font('Fonts/Sixtyfour.TTF', 20)
 font_widgets = pygame.font.Font('Fonts/Sixtyfour.TTF', 15)
 font_textplano = pygame.font.Font('Fonts/8-BIT WONDER.TTF', 18)
 
-#Funciones
-
 #Widgets
     #Widgets de la Pantalla Principal
 Button_Option = Button('Opciones', 250, 50, (260, 400), font_widgets, 6)
@@ -35,17 +33,28 @@ SubTitle = Text('de Conway', font_other, '#000000', 90, 190)
 StartText = Text('Presione SPACE para comenzar', font_other, '#000000', 110, 340)
     #Widgets de las Opciones
 Button_Back = Button('Atras', 100, 50, (20, 20), font_widgets, 6)
-
+    #Widgets de Inicio del Juego
+Button_Exit = Button('Atras', 100, 50, (20, 520), font_widgets, 6)
+Button_Paused = Button('Pausar', 125, 50, (315, 520), font_widgets, 6)
 #Game Variables
 game_option = False
 game_start = False
+game_paused = False
 ncX, ncY = 50, 50
 dimCW = SCREEN_WIDTH / ncX
 dimCH = SCREEN_HEIGHT / ncY
+gameState = np.zeros((ncX, ncY))
+gameState[21, 21] = 1
+gameState[22, 22] = 1
+gameState[22, 23] = 1
+gameState[21, 23] = 1
+gameState[20, 23] = 1
 
 #Game Loop
 while running: 
+    newGameState = np.copy(gameState)
     screen.fill('#DCDDD8')
+
     if Button_Option.pressed == True:
         Button_Back.draw(screen)
         if Button_Back.pressed == True: 
@@ -53,23 +62,57 @@ while running:
             Button_Back.pressed = False
         pass
     elif game_start == True:
-        for y in range(4, ncX-10):
+        time.sleep(0.045)
+        
+        Button_Paused.draw(screen)
+        Button_Exit.draw(screen)
+        if Button_Exit.checked_click() == True:
+            game_start = False
+
+        for event in pygame.event.get():
+            mouseEvent = pygame.mouse.get_pressed()
+            if sum(mouseEvent) > 0: 
+                posX, posY = pygame.mouse.get_pos()
+                celX, celY = int(np.floor(posX / dimCW)), int(np.floor(posY / dimCH)) 
+                newGameState[celX, celY] = 1
+          
+        for y in range(2, ncX-10):
             for x in range(2, ncY-2):
+                if not game_paused: 
+                    n_Neigh=gameState[(x - 1) % ncX, (y - 1) % ncY] + \
+                            gameState[(x)     % ncX, (y - 1) % ncY] + \
+                            gameState[(x + 1) % ncX, (y - 1) % ncY] + \
+                            gameState[(x - 1) % ncX, (y)     % ncY] + \
+                            gameState[(x + 1) % ncX, (y)     % ncY] + \
+                            gameState[(x - 1) % ncX, (y + 1) % ncY] + \
+                            gameState[(x)     % ncX, (y + 1) % ncY] + \
+                            gameState[(x + 1) % ncX, (y + 1) % ncY] 
+
+                    #Regla 1
+                    if gameState[x, y] == 0 and n_Neigh == 3: 
+                        newGameState[x, y] = 1
+                    #Regla 2
+                    elif gameState[x, y] == 1 and (n_Neigh < 2 or n_Neigh > 3): 
+                        newGameState[x, y] = 0
 
                 poly = [((x)     * dimCW, y       * dimCH),  
                         ((x + 1) * dimCW, y       * dimCH), 
                         ((x + 1) * dimCW, (y + 1) * dimCH), 
                         ((x)     * dimCW, (y + 1) * dimCH)]
                 
-                pygame.draw.polygon(screen, "#000000", poly, 1)
-        
+                if newGameState[x, y] == 0: 
+                    pygame.draw.polygon(screen, '#606060', poly, 1)
+                else:
+                    pygame.draw.polygon(screen, '#000000', poly, 0)
+                
+        gameState = np.copy(newGameState)
         pass
     else:
         TitleCenter.Draw(screen)
         SubTitle.Draw(screen)
         StartText.Draw(screen)
         Button_Option.draw(screen)
-    
+
     # Event Handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -79,6 +122,6 @@ while running:
                 game_start = True
 
     pygame.display.update()
+    clock.tick(60)
     
-
 pygame.quit()
